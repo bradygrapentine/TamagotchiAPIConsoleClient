a563f3df936d9c5dfbafacc875127351ef4085d1
bradygrapentine <brady.grapentine@gmail.com>
2021-05-09 15:06:07 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TamagotchiAPIConsoleClient
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# TamagotchiAPIConsoleClient
